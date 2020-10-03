3bb44df05942ee2f7c43f4572c318ba1dfce56c7
Alexandur06 <72245870+Alexandur06@users.noreply.github.com>
2020-10-03 17:27:21 +0300

    Update apiMethods.dart

--- lib/screens/apiMethods.dart
+++ lib/screens/apiMethods.dart
@@ -28,51 +28,80 @@ class apiCommunicator1 {
     }
   }
 
+  Future<String> registerDealer(String name, String address, String bussiness_hours, String description, String password) async {
+    const platformMethodChannel = const MethodChannel('walkly/native');
+
+    String _message;
+    try {
+      final String result =
+          await platformMethodChannel.invokeMethod('registerDealer', {
+        "name": name,
+        "address": address,
+        "bussiness_hours": bussiness_hours,
+        "description": description,
+        "password": password
+        });
+      _message = result;
+    } on PlatformException catch (e) {
+      _message = "Unable to use native functions!";
+    }
+    return _message;
+  }
   Future<String> registerUser(String first_name, String last_name, String email,
       String password) async {
-    //const platformMethodChannel = const MethodChannel('walkly/native');
+    const platformMethodChannel = const MethodChannel('walkly/native');
 
-    /*String _message;
+    String _message;
     try {
       final String result =
           await platformMethodChannel.invokeMethod('registerUser', {
         "first_name": first_name,
         "last_name": last_name,
         "email": email,
-        "password": password,
-        "context": context
+        "password": password
       });
       _message = result;
     } on PlatformException catch (e) {
       _message = "Unable to use native functions!";
-    }*/
-    var result =
-        await http.post('http://192.168.1.9/walklyapp/register.php', body: {
-      "first_name": first_name,
-      "last_name": last_name,
-      "email": email,
-      "password": password,
-    });
-    print(result);
-    return json.decode(result.body);
+    }
+    return _message;
   }
 
   Future<int> deleteProfile(String email) async {
     String url = this.serverAddress + 'deleteProfile';
-    var response = await http.post(Uri.parse(url));
+    var response = await http.post(Uri.parse(url), body: {'set-cookie': this.cookie});
     return jsonDecode(response.body);
   }
-
-  Future<Map> getOffers() {
-    //TODO Da vzima spisuk s oferti
+  Future<Map> getOffers() async {
+    String url = this.serverAddress + 'getOffers';
+    var response = await http.get(Uri.parse(url), body: {'set-cookie': this.cookie});
+    return jsonDecode(response.body);
   }
-  Future<Map> useOffer() {
-    //TODO Da polzva oferta (user)
+  Future<Map> useOffer(int id) async {
+    String url = this.serverAddress + 'useOffer';
+    var response = await http.post(Uri.parse(url), body: {'set-cookie': this.cookie,
+    'offerID': id});
+    return jsonDecode(response.body);
   }
-  Future<Map> makeOffer() {
-    //TODO Da pravi nova oferta (dealer)
+  Future<Map> makeOffer({int categoryID, String firmName, String describtion,
+   String address, String price, int unitsPerOffer, String bussinessHours, String dateFromToDateDue}) async {
+    String url = this.serverAddress + 'makeOffer';
+    var response = await http.post(Uri.parse(url), body: {'set-cookie': this.cookie,
+    "categoryID": categoryID,
+    "firmName": firmName,
+    "describtion": describtion,
+    "address": address,
+    "price": price,
+    "unitsPerOffer": unitsPerOffer,
+    "bussinessHours": bussinessHours,
+    "dateFromToDateDue": dateFromToDateDue:
+    });
+    return jsonDecode(response.body);
   }
-  Future<Map> deleteOffer() {
-    //TODO Da trie sobstvena oferta (dealer)
+  Future<Map> deleteOffer(int offerID) async {
+    String url = this.serverAddress + 'delteOffer';
+    var response = await http.post(Uri.parse(url), body: {'set-cookie': this.cookie,
+    "offerID": offerID});
+    return jsonDecode(response.body);
   }
 }
